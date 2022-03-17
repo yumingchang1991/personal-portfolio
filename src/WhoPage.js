53114add544f49d75f48233bfbe332e5517326eb
ChillDiver <yumingchang1991@gmail.com>
2022-03-17 17:03:51 +0800

    [feat] Add social media icon & link to whopage.js

--- src/WhoPage.js
+++ src/WhoPage.js
@@ -1,10 +1,29 @@
 import './WhoPage.css'
 
-function WhoPage() {
+export default function WhoPage() {
     return (
         <div className="who-content">
             <div className="avatar-container">
-                <img className="avatar" src="./Avatar.png" width="200px" height="211px" alt="this is avatar"></img>
+                <img className="avatar" src="./Avatar.png" width="188px" height="196px" alt="this is avatar"></img>
+            </div>
+            <div className='links-container'>
+                <ul className='social-media-links'>
+                    <li className='link-item'>
+                        <a className='link' href='https://www.linkedin.com/in/yumingchang1991/' target='_blank' rel='noreferrer'>
+                            <i class="fa-brands fa-linkedin fa-2xl"></i>
+                        </a>
+                    </li>
+                    <li className='link-item'>
+                        <a className='link' href='https://github.com/yumingchang1991' target='_blank' rel='noreferrer'>
+                            <i class="fa-brands fa-github-square fa-2xl"></i>
+                        </a>
+                    </li>
+                    <li className='link-item'>
+                        <a className='link' href='https://yumingchang1991.medium.com/' target='_blank' rel='noreferrer'>
+                            <i class="fa-brands fa-medium fa-2xl"></i>
+                        </a>
+                    </li>
+                </ul>
             </div>
             <section className="intro">
                 <p className="author">
@@ -24,4 +43,3 @@ function WhoPage() {
     )
 }
 
-export default WhoPage

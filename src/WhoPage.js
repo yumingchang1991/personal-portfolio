be3a6270471cfb281f6dc2482818859be55ffe72
ChillDiver <yumingchang1991@gmail.com>
2022-03-17 16:38:51 +0800

    Change CSS fontsize unit from px to rem

--- src/WhoPage.js
+++ src/WhoPage.js
@@ -4,7 +4,7 @@ function WhoPage() {
     return (
         <div className="who-content">
             <div className="avatar-container">
-                <img className="avatar" src="./Avatar.png" width="188px" height="194px" alt="this is avatar"></img>
+                <img className="avatar" src="./Avatar.png" width="200px" height="211px" alt="this is avatar"></img>
             </div>
             <section className="intro">
                 <p className="author">
@@ -12,12 +12,12 @@ function WhoPage() {
                         Yu-Ming, Chang
                     </author>
                 </p>
-                <p className="location">Taipei, Taiwan</p>
+                <p className="location">Taipei</p>
                 <p className="intro-summary">
                     JavaScript Developer
                 </p>
                 <p className="intro-summary">
-                    best with <em>React.js</em> and <em>Node.js</em>
+                    Essentialism Evanglist
                 </p>
             </section>
         </div>

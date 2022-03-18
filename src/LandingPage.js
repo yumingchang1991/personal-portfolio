d574ae1eeda4eb797b49b02225d29689342368fb
ChillDiver <yumingchang1991@gmail.com>
2022-03-18 17:12:53 +0800

    [fix] add padding 90px to WhoContent to visually align center

--- src/LandingPage.js
+++ src/LandingPage.js
@@ -7,7 +7,7 @@ function LandingPage() {
         <div className="landing-content">
             <h1 className="landing-title">{LandingTitle}</h1>
             <p className="landing-body">{LandingDescription}</p>
-            <i class="swipe-down-icon fa-solid fa-angles-down fa-2xl"></i>
+            {/* <i class="swipe-down-icon fa-solid fa-angles-down fa-2xl"></i> */}
         </div>
     )
 }

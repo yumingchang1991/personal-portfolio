e311015d5e2b29615214319da2a85c2a05dae2e5
ChillDiver <yumingchang1991@gmail.com>
2022-06-03 21:00:47 +0800

    feat: init projects page

---
+++ pages/projects.js
@@ -0,0 +1,10 @@
+import styles from '../styles/Projects.module.css'
+
+export default function Projects () {
+  return (
+    <div className={styles['projects-content']}>
+      <p>Placeholder for incoming</p>
+      <h1>Backend Projects</h1>
+    </div>
+  )
+}

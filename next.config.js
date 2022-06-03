ba44a16326e96993318713de7595143db5fa52ee
ChillDiver <yumingchang1991@gmail.com>
2022-06-03 21:57:54 +0800

    test: add basePath key to see if rrot URL is correct

--- next.config.js
+++ next.config.js
@@ -1,7 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  assetPrefix: process.env.NEXT_PUBLIC_BASE_PATH || ''
+  assetPrefix: process.env.NEXT_PUBLIC_BASE_PATH || '',
+  basePath: '/personal-portfolio'
 }
 
 module.exports = nextConfig

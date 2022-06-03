62b548e32181d18e016c6533133e9fab29869761
ChillDiver <yumingchang1991@gmail.com>
2022-06-03 21:49:45 +0800

    test: seems like assetPrefix has nothing to do with homepage keyword

--- next.config.js
+++ next.config.js
@@ -1,7 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  // assetPrefix: process.env.NEXT_PUBLIC_BASE_PATH || ''
+  assetPrefix: process.env.NEXT_PUBLIC_BASE_PATH || ''
 }
 
 module.exports = nextConfig

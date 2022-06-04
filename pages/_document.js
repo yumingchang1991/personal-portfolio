dbe33322389fee86080a83f23ef8b59792f97487
ChillDiver <yumingchang1991@gmail.com>
2022-06-04 21:05:48 +0800

    doc: add new favicon design to page

--- pages/_document.js
+++ pages/_document.js
@@ -15,7 +15,11 @@ export default class MyDocument extends Document {
             rel="stylesheet"
             href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700&display=swap"
           />
-          <link rel="icon" href="favicon.ico" />
+          {/* <link rel="icon" href="favicon/favicon.ico" /> */}
+          <link rel="apple-touch-icon" sizes="180x180" href="favicon/apple-touch-icon.png" />
+          <link rel="icon" type="image/png" sizes="32x32" href="favicon/favicon-32x32.png" />
+          <link rel="icon" type="image/png" sizes="16x16" href="favicon/favicon-16x16.png" />
+          <link rel="manifest" href="favicon/site.webmanifest" />
         </Head>
         <body>
           <Main />

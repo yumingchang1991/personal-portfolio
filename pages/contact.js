ea7c8a4168691a1b73293e43bcca9698131cbdb2
ChillDiver <yumingchang1991@gmail.com>
2022-06-08 17:18:24 +0800

    feat: add contact page

---
+++ pages/contact.js
@@ -0,0 +1,42 @@
+import { Typography } from '@mui/material';
+import Divider from '@mui/material/Divider';
+import styles from '../styles/Contact.module.css'
+
+const EMAIL = 'yumingchang1991@gmail.com'
+const LINKEDIN = 'https://www.linkedin.com/in/yumingchang1991/'
+
+export default function Contact() {
+  return (
+    <>
+      <h2>
+        Thank you for your interest!
+      </h2>
+      <Divider />
+      <p>
+        Pick your preference to reach out:
+      </p>
+      <ul className={styles['list-container']}>
+        <li className={styles['list-item']} onClick={copyEmailToClipboard}>
+          <email>{EMAIL}</email>
+        </li>
+        <li className={styles['list-item']} onClick={openInNewTab}>LinkedIn</li>
+      </ul>
+      <Divider />
+      <a
+        className={styles.link}
+        href='https://www.timeanddate.com/worldclock/converter.html?iso=20220608T080000&p1=241' target='_blank'>
+        Time Zone Converter
+      </a>
+    </>
+  )
+}
+
+function openInNewTab() {
+  window.open(LINKEDIN, '_blank')
+}
+
+function copyEmailToClipboard() {
+  navigator.clipboard
+    .writeText(EMAIL)
+    .then(() => alert('email is copied to clipboard!'))
+}

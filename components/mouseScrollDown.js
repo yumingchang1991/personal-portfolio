d66d4a6c2ffbf83d7f2c2019d0b881fa681c4afd
ChillDiver <yumingchang1991@gmail.com>
2022-09-06 23:17:00 +0800

    feat: add call toaction button to projects

--- components/mouseScrollDown.js
+++ components/mouseScrollDown.js
@@ -1,7 +1,7 @@
 import KeyboardArrowDownIcon from '@mui/icons-material/KeyboardArrowDown'
+import NavigateNextIcon from '@mui/icons-material/NavigateNext'
 import { Typography } from '@mui/material'
 import styles from '../styles/MouseScrollDown.module.css'
-
 
 export default function MouseScrollDown() {
   const textStyle = {
@@ -10,20 +10,33 @@ export default function MouseScrollDown() {
   
   return (
     <>
-      <div className={styles['scroll-down']} onClick={scrolldownOnClicked}>
-        <a>
-          <KeyboardArrowDownIcon />
-          <Typography sx={ textStyle }>scroll</Typography>
-        </a>
+      <div className={styles['call-to-actions']}>
+        <div className={styles['action-to-profile']} onClick={scrollToProfile}>
+          <div className={styles['arrow-down']}>
+            <KeyboardArrowDownIcon />
+          </div>
+          <Typography sx={ textStyle }>to Profile</Typography>
+        </div>
+        <div className={styles['action-to-project']} onClick={navigateToProjects}>
+          <Typography sx={textStyle}>to Projects</Typography>
+          <div className={styles['arrow-right']}>
+            <NavigateNextIcon />
+          </div>
+        </div>
       </div>
     </>
   )
 }
 
-function scrolldownOnClicked(e) {
+function scrollToProfile(e) {
   const homeMain = document.querySelector('#home-container')
   window.scrollTo({
     top: homeMain.clientHeight,
     behavior: 'smooth'
   })
+}
+
+function navigateToProjects(e) {
+  const url = new URL(`${document.location.href}/projects`)
+  window.location.assign(url)
 }
